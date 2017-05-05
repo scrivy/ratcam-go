b09aa43e548b955badbb8f244602ba277b21184f
Daniel Scrivano <daniel@danielscrivano.com>
2017-05-05 00:55:31 -0700

    somewhat works

---
+++ application.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	index, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		log.Println(err)
+		log.Fatalln("problem loading index.html")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, http.StatusText(405), 405)
+			return
+		}
+
+		w.Header().Set("Cache-Control", "max-age=600")
+		w.Write(index)
+	})
+
+	http.HandleFunc("/latest.jpg", func(w http.ResponseWriter, r *http.Request) {
+
+		var imageBytes bytes.Buffer
+		start := time.Now()
+		cmd := exec.Command("fswebcam", "-")
+		cmd.Stdout = &imageBytes
+		err := cmd.Run()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		elapsed := time.Since(start)
+		log.Printf("captured image in %s", elapsed)
+
+		w.Write(imageBytes.Bytes())
+	})
+
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
