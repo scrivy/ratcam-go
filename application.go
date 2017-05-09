75cd68583a1d2b13727ba3b19e4e896559738922
Daniel Scrivano <daniel@danielscrivano.com>
2017-05-09 12:02:18 -0700

    can serve more people now

--- application.go
+++ application.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -15,6 +19,8 @@ func main() {
 		log.Println(err)
 		log.Fatalln("problem loading index.html")
 	}
+
+	go takePictures()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
@@ -27,26 +33,60 @@ func main() {
 	})
 
 	http.HandleFunc("/latest.jpeg", func(w http.ResponseWriter, r *http.Request) {
+		picBytes, err := getLatestPicture()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.Header().Set("Content-Type", "image/jpeg")
+		w.Write(picBytes)
+	})
 
-		var imageBytes, stdErr bytes.Buffer
-		start := time.Now()
-		cmd := exec.Command("fswebcam", "-r", "1280x720", "--jpeg", "90", "-")
+	log.Println("Listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+var (
+	latestPicture []byte
+	mutex         *sync.RWMutex = &sync.RWMutex{}
+)
+
+func takePictures() {
+	var imageBytes, stdErr bytes.Buffer
+	var start time.Time
+	for {
+		start = time.Now()
+		cmd := exec.Command("fswebcam", "-r", "1280x720", "--jpeg", "90", "-q", "-")
 		cmd.Stdout = &imageBytes
 		cmd.Stderr = &stdErr
 		err := cmd.Run()
 		if err != nil {
 			log.Println(err)
-			return
+			continue
 		}
 		elapsed := time.Since(start)
 		log.Printf("captured image in %s", elapsed)
-		if stdErr.Len() > 0 {
-			log.Printf("exec error: %s\n", stdErr.String())
+		fmt.Printf("\n%s\n", stdErr.String())
+		if strings.Contains(stdErr.String(), "unrecoverable error") {
+			imageBytes.Reset()
+			stdErr.Reset()
+			continue
 		}
-		w.Header().Set("Content-Type", "image/jpeg")
-		w.Write(imageBytes.Bytes())
-	})
+		mutex.Lock()
+		latestPicture = imageBytes.Bytes()
+		mutex.Unlock()
+		imageBytes.Reset()
+		stdErr.Reset()
+	}
 
-	log.Println("Listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getLatestPicture() (picture []byte, err error) {
+	mutex.RLock()
+	picture = latestPicture
+	mutex.RUnlock()
+	if len(picture) == 0 {
+		err = errors.New("bad gorilla")
+	}
+	return
 }
