622aaf457f9e0bfc6f0fecf12e65b8ef3f3a499f
Daniel Scrivano <daniel@danielscrivano.com>
2017-05-14 03:52:07 -0700

    works so much better

--- application.go
+++ application.go
@@ -11,25 +11,31 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/getsentry/raven-go"
 )
 
 func main() {
 	index, err := ioutil.ReadFile("index.html")
 	if err != nil {
-		log.Println(err)
+		logErr(err)
 		log.Fatalln("problem loading index.html")
 	}
 
-	go takePictures()
+	incoming := make(chan bool, 50)
+
+	go takePictures(incoming)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.Error(w, http.StatusText(405), 405)
 			return
 		}
-
+		expires := time.Now().Add(5 * time.Minute).Format(http.TimeFormat)
+		w.Header().Set("Expires", expires)
 		w.Header().Set("Cache-Control", "max-age=600")
 		w.Write(index)
+		incoming <- true
 	})
 
 	http.HandleFunc("/latest.jpeg", func(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +46,7 @@ func main() {
 		}
 		w.Header().Set("Content-Type", "image/jpeg")
 		w.Write(picBytes)
+		incoming <- true
 	})
 
 	log.Println("Listening on :8080")
@@ -48,49 +55,68 @@ func main() {
 
 var (
 	latestPicture []byte
-	mutex         *sync.RWMutex = &sync.RWMutex{}
+	pictureMutex  *sync.RWMutex = &sync.RWMutex{}
 )
 
-func takePictures() {
+func takePictures(incomingReq chan bool) {
 	var imageBytes, stdErr bytes.Buffer
 	var start time.Time
 	var err error
+	countDown := uint32(1)
 	for {
-		start = time.Now()
-		cmd := exec.Command("fswebcam", "-r", "1280x720", "--jpeg", "90", "-q", "-")
-		cmd.Stdout = &imageBytes
-		cmd.Stderr = &stdErr
-		err = cmd.Run()
-		if err != nil {
-			log.Println(err)
-			continue
+		select {
+		case <-incomingReq:
+			countDown++
+			log.Printf("countDown: %d\n", countDown)
+		default:
+			if countDown > 0 {
+				start = time.Now()
+				cmd := exec.Command("fswebcam", "-r", "1920x1080", "--jpeg", "90", "-q", "-")
+				cmd.Stdout = &imageBytes
+				cmd.Stderr = &stdErr
+				err = cmd.Run()
+				if err != nil {
+					logErr(err)
+					continue
+				}
+				elapsed := time.Since(start)
+				fmt.Printf("\n%s\n", stdErr.String())
+				if strings.Contains(stdErr.String(), "unrecoverable error") {
+					logErr(errors.New(stdErr.String()))
+				} else if strings.Contains(stdErr.String(), "Error opening device") {
+					logErr(errors.New(stdErr.String()))
+				} else if strings.Contains(stdErr.String(), "No such file or directory") {
+					logErr(errors.New(stdErr.String()))
+				} else if len(stdErr.String()) > 0 {
+					logErr(errors.New(stdErr.String()))
+				} else {
+					pictureMutex.Lock()
+					latestPicture = imageBytes.Bytes()
+					pictureMutex.Unlock()
+					log.Printf("captured image in %s", elapsed)
+				}
+				imageBytes.Reset()
+				stdErr.Reset()
+				countDown--
+				log.Printf("countDown: %d\n", countDown)
+			} else {
+				time.Sleep(500 * time.Millisecond)
+			}
 		}
-		elapsed := time.Since(start)
-		fmt.Printf("\n%s\n", stdErr.String())
-		if strings.Contains(stdErr.String(), "unrecoverable error") {
-
-		} else if strings.Contains(stdErr.String(), "Error opening device") {
-
-		} else if strings.Contains(stdErr.String(), "No such file or directory") {
-
-		} else {
-			mutex.Lock()
-			latestPicture = imageBytes.Bytes()
-			mutex.Unlock()
-			log.Printf("captured image in %s", elapsed)
-		}
-		imageBytes.Reset()
-		stdErr.Reset()
-		time.Sleep(6 * time.Second)
 	}
 }
 
 func getLatestPicture() (picture []byte, err error) {
-	mutex.RLock()
+	pictureMutex.RLock()
 	picture = latestPicture
-	mutex.RUnlock()
+	pictureMutex.RUnlock()
 	if len(picture) == 0 {
 		err = errors.New("bad gorilla")
 	}
 	return
+}
+
+func logErr(err error) {
+	log.Println(err)
+	raven.CaptureErrorAndWait(err, nil)
 }
