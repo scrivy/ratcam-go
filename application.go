b47bb81857674491d3ebdd788158814ef2170a01
Daniel Scrivano <daniel@danielscrivano.com>
2018-02-05 23:14:54 -0800

    removed some code

--- application.go
+++ application.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os/exec"
-	"strings"
 	"sync"
 	"time"
-
-	"github.com/getsentry/raven-go"
 )
 
 func main() {
 	index, err := ioutil.ReadFile("index.html")
 	if err != nil {
-		logErr(err)
-		log.Fatalln("problem loading index.html")
+		panic(err)
 	}
 
 	go takePictures()
@@ -60,6 +54,7 @@ func takePictures() {
 	var imageBytes, stdErr bytes.Buffer
 	var start time.Time
 	var err error
+	var cmd *exec.Cmd
 	for {
 		lastRequestLock.RLock()
 		if !lastRequest.Add(5 * time.Second).After(time.Now()) {
@@ -71,35 +66,23 @@ func takePictures() {
 
 		pictureMutex.Lock()
 		start = time.Now()
-		cmd := exec.Command("fswebcam", "-r", "1920x1080", "--jpeg", "90", "-q", "--no-banner", "-")
+		cmd = exec.Command("fswebcam", "-r", "1920x1080", "--jpeg", "90", "-q", "--no-banner", "-")
 		cmd.Stdout = &imageBytes
 		cmd.Stderr = &stdErr
 		err = cmd.Run()
 		if err != nil {
-			logErr(err)
+			pictureMutex.Unlock()
+			log.Println(err.Error())
 			continue
 		}
-		elapsed := time.Since(start)
-		fmt.Printf("\n%s\n", stdErr.String())
-		if strings.Contains(stdErr.String(), "unrecoverable error") {
-			logErr(errors.New(stdErr.String()))
-		} else if strings.Contains(stdErr.String(), "Error opening device") {
-			logErr(errors.New(stdErr.String()))
-		} else if strings.Contains(stdErr.String(), "No such file or directory") {
-			logErr(errors.New(stdErr.String()))
-		} else if len(stdErr.String()) > 0 {
-			logErr(errors.New(stdErr.String()))
+		if stdErr.Len() > 0 {
+			log.Println(stdErr.String())
 		} else {
 			latestPicture = imageBytes.Bytes()
-			log.Printf("captured image in %s", elapsed)
+			log.Printf("captured image in %s", time.Since(start).String())
 		}
 		pictureMutex.Unlock()
 		imageBytes.Reset()
 		stdErr.Reset()
 	}
-}
-
-func logErr(err error) {
-	log.Println(err)
-	raven.CaptureErrorAndWait(err, nil)
 }
